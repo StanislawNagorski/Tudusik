a0600582bcd5320b7c2a5c4d81f013b1c7b46cb5
stah <stanislaw.nagorski@gmail.com>
2021-04-19 21:15:23 +0200

    tasksList and SingleTask extracted from screen to components

--- lib/components/task_single.dart
+++ lib/components/task_single.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
 class SingleTask extends StatelessWidget {
-  const SingleTask({
-    Key key,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
