9e774e622c6e4abe449a89188421daf782f60420
stah <stanislaw.nagorski@gmail.com>
2021-04-19 20:40:21 +0200

    added: space where todos will be added

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,35 +5,48 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, bottom: 30.0, left: 30.0, right: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              child: Icon(
-                Icons.list,
-                size: 60.0,
-                color: Colors.lightBlueAccent,
-              ),
-              radius: 40.0,
-              backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, bottom: 30.0, left: 30.0, right: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 60.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  radius: 40.0,
+                  backgroundColor: Colors.white,
+                ),
+                SizedBox(height: 30.0),
+                Text(
+                  'Tudusik',
+                  style: TextStyle(
+                      fontSize: 50.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                )
+              ],
             ),
-            SizedBox(height: 30.0),
-            Text(
-              'Tudusik',
-              style: TextStyle(
-                  fontSize: 50.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700),
-            ),
-            Text(
-              '12 Tasks',
-              style: TextStyle(color: Colors.white, fontSize: 20.0),
-            )
-          ],
-        ),
+          ),
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0))),
+          ))
+        ],
       ),
     );
   }
