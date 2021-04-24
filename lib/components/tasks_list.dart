a784d5cbbf5f02ee13afd005f75cc91e37e704ea
stah <stanislaw.nagorski@gmail.com>
2021-04-24 20:34:10 +0200

    TasksList wrapped with Consumer

--- lib/components/tasks_list.dart
+++ lib/components/tasks_list.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do/components/task_single.dart';
-import 'package:to_do/models/Task.dart';
 import 'package:to_do/models/user_tasks.dart';
-
 
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Task> taskList = Provider.of<UserTasks>(context).list;
-
-    return ListView.builder(
-      itemCount: taskList.length,
-      itemBuilder: (context, index) {
-        return SingleTask(
-          task: taskList[index],
-          checkBoxCallBack: (bool value) {
-            Provider.of<UserTasks>(context, listen: false).checkTask(index);
-            // setState(() {
-            //   taskList[index].toggleCheckBox();
-            // });
-          },
-        );
-      },
-    );
+    return Consumer<UserTasks>(builder: (context, taskData, child) {
+      return ListView.builder(
+          itemCount: taskData.getNumberOfTasks(),
+          itemBuilder: (context, index) {
+            return SingleTask(
+              task: taskData.list[index],
+              checkBoxCallBack: (bool value) {
+                taskData.checkTask(index);
+              },
+            );
+          });
+    });
   }
 }
